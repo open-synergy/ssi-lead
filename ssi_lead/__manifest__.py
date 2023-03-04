a782f34d8abf4b98ec58372af46249356486ede1
Andhitia Rama <andhitia.r@gmail.com>
2023-03-05 06:37:33 +0700

    [ssi_lead] 14.0.1.0.0

---
+++ ssi_lead/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Lead Enhancement",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "crm",
+        "ssi_master_data_mixin",
+    ],
+    "data": [],
+    "demo": [],
+}
