40d5f15e6aa7097bcf4365769e6df2a5550f6426
Andhitia Rama <andhitia.r@gmail.com>
2023-10-10 18:50:26 +0700

    [UPD] ssi_lead_risk_analysis
    * Add risk analysis onchange

--- ssi_lead_risk_analysis/models/crm_lead.py
+++ ssi_lead_risk_analysis/models/crm_lead.py
@@ -2,7 +2,7 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
 
-from odoo import models
+from odoo import api, models
 
 
 class CrmLead(models.Model):
@@ -13,3 +13,8 @@ class CrmLead(models.Model):
     ]
     _risk_analysis_create_page = True
     _risk_analysis_partner_field_name = "partner_id"
+
+    @api.onchange("partner_id")
+    def onchange_risk_analysis_id(self):
+        _super = super(CrmLead, self)
+        _super.onchange_risk_analysis_id()
