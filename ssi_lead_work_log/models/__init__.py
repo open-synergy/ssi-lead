ac691f10fcc440c39cc5a2a3466745c95956ba4a
Andhitia Rama <andhitia.r@gmail.com>
2026-01-16 09:40:23 +0700

    [UPD] ssi_lead_work_log
    * Pre-commit fix

--- ssi_lead_work_log/models/__init__.py
+++ ssi_lead_work_log/models/__init__.py
@@ -2,6 +2,6 @@
 # Copyright 2022 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
 
-from . import (
+from . import (  # noqa: F401
     helpdesk_ticket,
 )
