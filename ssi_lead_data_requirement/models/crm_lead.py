11ffcefa20ffafc4b86c683254fd0822a5232802
Andhitia Rama <andhitia.r@gmail.com>
2026-04-30 00:42:42 +0700

    [UPD] ssi_lead_data_requirement
    * Remove data_requirement_ids M2M redefinition — now inherited from mixin via data_requirement.document
    * Migration script for 14.0.1.2.0

--- ssi_lead_data_requirement/models/crm_lead.py
+++ ssi_lead_data_requirement/models/crm_lead.py
@@ -2,10 +2,10 @@
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
 
-from odoo import fields, models
+from odoo import models
 
 
-class CrmLead(models.Model):
+class CrmLead(models.Model):  # pylint: disable=too-few-public-methods
     _name = "crm.lead"
     _inherit = [
         "crm.lead",
@@ -13,9 +13,3 @@ class CrmLead(models.Model):
     ]
     _data_requirement_create_page = True
     _data_requirement_partner_field_name = "partner_id"
-
-    data_requirement_ids = fields.Many2many(
-        relation="rel_lead_2_data_requirement",
-        column1="lead_id",
-        column2="data_requirement_id",
-    )
