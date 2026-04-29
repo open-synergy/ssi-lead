02017ec3d3a84b12df11b3278d3b4baf03346d4c
Andhitia Rama <andhitia.r@gmail.com>
2026-04-30 01:56:14 +0700

    [UPD] ssi_lead_project
    * Fix pylint too-few-public-methods

--- ssi_lead_project/models/crm_lead.py
+++ ssi_lead_project/models/crm_lead.py
@@ -5,7 +5,7 @@
 from odoo import fields, models
 
 
-class CrmLead(models.Model):
+class CrmLead(models.Model):  # pylint: disable=too-few-public-methods
     _name = "crm.lead"
     _inherit = [
         "crm.lead",
