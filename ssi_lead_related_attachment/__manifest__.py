0e774e1083b6830592965c53daea1ae94e71f095
Andhitia Rama <andhitia.r@gmail.com>
2026-07-21 23:22:03 +0700

    [ADD] ssi_lead_related_attachment
    Modul glue baru yang memasang mixin.related_attachment pada crm.lead
    agar dokumen pendaftaran dapat dilampirkan per kategori mengikuti
    template, dengan status verifikasi terbaca dari satu field
    related_attchment_status (not_needed/open/done). Termasuk unit test
    skenario YAML yang menguji siklus status lampiran.

---
+++ ssi_lead_related_attachment/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2026 OpenSynergy Indonesia
+# Copyright 2026 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+{
+    "name": "Lead - Related Attachment Integration",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "ssi_lead",
+        "ssi_related_attachment_mixin",
+    ],
+    "data": [],
+    "demo": [],
+    "images": [],
+}
